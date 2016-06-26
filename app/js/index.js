89151108c7135a951c625d3db54e37541d4bebd9
Kamilla <venessiel@gmail.com>
2016-06-26 14:10:13 -0400

    minified views

--- app/js/index.js
+++ app/js/index.js
@@ -3,21 +3,16 @@ $(document).ready(function() {
   $("button.set").click(function() {
     var value = $("input.text").val();
     MeshWall.sendMessage(value);
-  });
-
-  $("button.get-one").click(function() {
-    var value = MeshWall.getMessages();
-    console.log('messages value',web3.toAscii(value[0]));
-    $(".value").html(value);
+    console.log('send new message to blockchain', val);
   });
 
   $("button.get-all").click(function() {
     var messages = MeshWall.getMessages();
     console.log('messages', messages)
-    var value = messages.map(function(message){
+    var messageStrings = messages.map(function(message){
       return web3.toAscii(message);
     });
-    console.log('messages value',web3.toAscii(value[0]));
+    console.log('messages return', messageStrings);
     $(".value").html(value.join(', \n'));
   });
 
